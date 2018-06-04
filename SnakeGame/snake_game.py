e089080c56267205774d4736da34999e8dc2da66
Blockchain63 <37481272+Blockchain63@users.noreply.github.com>
2018-06-04 15:29:02 +0400

    Update snake_game.py

--- SnakeGame/snake_game.py
+++ SnakeGame/snake_game.py
@@ -1,31 +1,29 @@
-# -*- coding: UTF-8 -*-
 import curses
 from random import randint
 
 
 class SnakeGame:
-    #"Конструктор"
-    def __init__(self, board_width = 20, board_height = 20, gui = False):
+    # "Конструктор"
+    def __init__(self, board_width=25, board_height=25, gui=False):
         self.score = 0
-        self.done = False
+        self.is_done = False
         self.board = {'width': board_width, 'height': board_height}
         self.gui = gui
 
     def start(self):
-        self.snake_init()
+        self.init_snake()
         self.generate_food()
         if self.gui:
             self.render_init()
         return self.generate_observations()
 
-    def snake_init(self):
+    def init_snake(self):
         x = randint(5, self.board["width"] - 5)
         y = randint(5, self.board["height"] - 5)
-        #Массив из частей змейки
+        # Массив из частей змейки
         self.snake = []
-        vertical = randint(0,1) == 0
         for i in range(3):
-            point = [x + i, y] if vertical else [x, y + i]
+            point = [x, y + i]
             self.snake.insert(0, point)
 
     def generate_food(self):
@@ -36,41 +34,42 @@ class SnakeGame:
         self.food = food
 
     def render_init(self):
-        curses.initscr() #инициализация библиотеки
+        curses.initscr()  # инициализация библиотеки
         # возвращает окно с левым верхним углом в (0,0) с размерами width+2 height+2
-        win = curses.newwin(self.board["width"] + 2, self.board["height"] + 2, 0, 0)
-        #делает курсор невидимым
+        window = curses.newwindow(self.board["width"] + 2, self.board["height"] + 2, 0, 0)
+        # делает курсор невидимым
         curses.curs_set(0)
-        #getch() не блокирует
-        win.nodelay(1)
-        #getch() блокирует на 200 мс
-        win.timeout(200)
-        self.win = win
+        # getch() не блокирует
+        window.nodelay(1)
+        # getch() блокирует на 200 мс
+        window.timeout(200)
+        self.window = window
         self.render()
 
     def render(self):
-        self.win.clear()
-        self.win.border(0)
-        self.win.addstr(0, 2, 'Score : ' + str(self.score) + 'Q')
-        self.win.addch(self.food[0], self.food[1], 'Q')
+        self.window.clear()
+        self.window.border(0)
+        self.window.addstr(0, 2, 'Score : ' + str(self.score))
+        self.window.addch(self.food[0], self.food[1], 'Q')
         for i, point in enumerate(self.snake):
             if i == 0:
-                #Голова змеи
-                self.win.addch(point[0], point[1], '@')
+                # Голова змеи
+                self.window.addch(point[0], point[1], '@')
             else:
-                #Остальные куски змеи
-                self.win.addch(point[0], point[1], 'o')
-        #Получение кнопки
-        self.win.getch()
+                # Остальные куски змеи
+                self.window.addch(point[0], point[1], 'o')
+        # Получение кнопки
+        self.window.getch()
 
     def step(self, key):
-        # 0 - UP
-        # 1 - RIGHT
-        # 2 - DOWN
-        # 3 - LEFT
-        if self.done == True:
+        # Клавиши
+        # 0 - Вверх
+        # 1 - Вправо
+        # 2 - Вниз
+        # 3 - Влево
+        if self.is_done:
             self.end_game()
-        self.create_new_point(key)
+        self.add_point(key)
         if self.food_eaten():
             self.score += 1
             self.generate_food()
@@ -80,7 +79,7 @@ class SnakeGame:
         if self.gui: self.render()
         return self.generate_observations()
 
-    def create_new_point(self, key):
+    def add_point(self, key):
         new_point = [self.snake[0][0], self.snake[0][1]]
         if key == 0:
             new_point[0] -= 1
@@ -99,25 +98,21 @@ class SnakeGame:
         return self.snake[0] == self.food
 
     def check_collisions(self):
+        # Если змейка совершив ход, врежется в одну из границ
         if (self.snake[0][0] == 0 or
-            self.snake[0][0] == self.board["width"] + 1 or
-            self.snake[0][1] == 0 or
-            self.snake[0][1] == self.board["height"] + 1 or
-            self.snake[0] in self.snake[1:-1]):
-            self.done = True
+                self.snake[0][0] == self.board["width"] + 1 or
+                self.snake[0][1] == 0 or
+                self.snake[0][1] == self.board["height"] + 1 or
+                # Не врежется в одну из своих точек(кроме хвоста) 
+                self.snake[0] in self.snake[1:-1]):
+            self.is_done = True
 
     def generate_observations(self):
-        return self.done, self.score, self.snake, self.food
+        return self.is_done, self.score, self.snake, self.food
 
-    def render_destroy(self):
-        curses.endwin()
+    def render_end(self):
+        curses.endwindow()
 
     def end_game(self):
-        if self.gui: self.render_destroy()
+        if self.gui: self.render_end()
         raise Exception("Game over")
-
-if __name__ == "__main__":
-    game = SnakeGame(gui = True)
-    game.start()
-    for i in range(200):
-        game.step(randint(0,3))
